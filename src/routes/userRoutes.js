ba2d4b4f658c4d79212cd716e9281ac7f40ecbbb
Arnonerp <arnon.shir@gmail.com>
2024-12-04 16:45:21 +0200

    Add user registration get response for test

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -2,6 +2,11 @@ const express = require('express');
 const User = require('../models/userModel');
 const router = express.Router();
 
+router.get('/register', (req, res) => {
+    res.json({ message: 'User registration endpoint (GET)' });
+});
+
+
 router.post('/register', async (req, res) => {
     const user = new User(req.body);
     await user.save();
